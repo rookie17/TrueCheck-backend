7f5cd4f38a66cd0aefbfcd11934af7254fcf12c7
Aaditya <rookieaadi17@gmail.com>
2025-04-17 03:34:04 +0530

    docs:updated SYSTEM_PROMPT

--- utils/openai_client.py
+++ utils/openai_client.py
@@ -1,29 +1,24 @@
 from config import OPENAI_API_KEY
-
 from openai import OpenAI
 
 client = OpenAI(api_key=OPENAI_API_KEY)
 
 def get_ingredient_details_from_openai(ingredient_name):
-    prompt = f"""
-You are a health analyst AI. Rate the following ingredient based on health effects.
 
-Ingredient: {ingredient_name}
+    with open("system_prompt.txt", "r") as file:
+        system_prompt = file.read()
 
-Give your answer in JSON format with these keys:
-- pros
-- cons
-- health_score (1-10)
-- description
-    """
-    
+
+    user_prompt = ingredient_name
+
+    # Send to OpenAI
     response = client.chat.completions.create(
-        model="gpt-4o",
+        model="gpt-4o-mini",
         messages=[
-            {"role": "system", "content": "You are a helpful assistant that gives structured ingredient health analysis."},
-            {"role": "user", "content": prompt}
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt}
         ],
-        temperature=0.7
+        temperature=0.4 
     )
 
     return response.choices[0].message.content
